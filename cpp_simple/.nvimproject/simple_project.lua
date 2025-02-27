a0bc553ce393211bb74f689f36d01dd2569464bd
Stefan Thor Bjarnason <stefanttb@gmail.com>
2025-02-27 19:42:03 +0000

    retab.

--- cpp_simple/.nvimproject/simple_project.lua
+++ cpp_simple/.nvimproject/simple_project.lua
@@ -2,33 +2,33 @@ local utils = require("projects.utils")
 local root = utils.local_project_path(utils.script_path())
 
 local M = {
-	root_dir = root,
+    root_dir = root,
 
-	extensions = {
-		builds = {
-			build = {
-				executor = "vim",
-				compiler = "gcc",
-				makeprg = "make",
-				command = "builder make",
-				abortcommand = "AbortDispatch",
-			},
-		},
-		lspconfig = {
-			clangd = {
-				lsp_root = root,
-				cache_subdir = 'simple',
-				compdb_dir = root,
-			},
-		},
+    extensions = {
+        builds = {
+            build = {
+                executor = "vim",
+                compiler = "gcc",
+                makeprg = "make",
+                command = "builder make",
+                abortcommand = "AbortDispatch",
+            },
+        },
+        lspconfig = {
+            clangd = {
+                lsp_root = root,
+                cache_subdir = 'simple',
+                compdb_dir = root,
+            },
+        },
         sessions = {
             session_name = root:gsub('/', '_'),
         },
-	},
+    },
 }
 
 function M.on_project_open()
-	vim.opt.makeprg = "make"
+    vim.opt.makeprg = "make"
 end
 
 function M.on_project_close()
